b80fcb850e443849455ef7ca883bfdef643bbcde
Heba <36724067+heba@users.noreply.github.com>
2020-10-18 14:00:50 +0300

    from datetime to date and time

--- src/data.js
+++ src/data.js
@@ -12,7 +12,8 @@ const classes = [
         id:1,
         title: "class name",
         type:"cardio",
-        datetime: "today",
+        date: "today",
+        time: "at 5",
         isFree: true,
         capacity: 20,
         gym: gyms[0]
@@ -21,7 +22,8 @@ const classes = [
         id:2,
         title: "class name 2",
         type:"walking",
-        datetime: "tomorrow",
+        date: "tomorrow",
+        time: "at 8",
         isFree: false,
         capacity: 10,
         gym: gyms[1]
